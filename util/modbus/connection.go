1ba4cfde3e1385ef2b39036aa9ba66eef1f74e0d
cbshopper <5838696+cbshopper@users.noreply.github.com>
2024-08-21 09:31:43 +0200

    Update connection.go

--- util/modbus/connection.go
+++ util/modbus/connection.go
@@ -59,55 +59,66 @@ func (c *Connection) prepare() {
 
 func (c *Connection) ReadCoils(address, quantity uint16) ([]byte, error) {
 	c.prepare()
+	defer c.Connection.Close() //!!!!
 	return c.ModbusClient().ReadCoils(address, quantity)
 }
 
 func (c *Connection) WriteSingleCoil(address, value uint16) ([]byte, error) {
 	c.prepare()
+	defer c.Connection.Close() //!!!!
 	return c.ModbusClient().WriteSingleCoil(address, value)
 }
 
 func (c *Connection) ReadInputRegisters(address, quantity uint16) ([]byte, error) {
 	c.prepare()
+	defer c.Connection.Close() //!!!!
 	return c.ModbusClient().ReadInputRegisters(address, quantity)
 }
 
 func (c *Connection) ReadHoldingRegisters(address, quantity uint16) ([]byte, error) {
 	c.prepare()
+	defer c.Connection.Close() //!!!!
 	return c.ModbusClient().ReadHoldingRegisters(address, quantity)
 }
 
 func (c *Connection) WriteSingleRegister(address, value uint16) ([]byte, error) {
 	c.prepare()
+	defer c.Connection.Close() //!!!!
 	return c.ModbusClient().WriteSingleRegister(address, value)
 }
 
 func (c *Connection) WriteMultipleRegisters(address, quantity uint16, value []byte) ([]byte, error) {
 	c.prepare()
+	defer c.Connection.Close() //!!!!
 	return c.ModbusClient().WriteMultipleRegisters(address, quantity, value)
 }
 
 func (c *Connection) ReadDiscreteInputs(address, quantity uint16) (results []byte, err error) {
 	c.prepare()
+	defer c.Connection.Close()  //!!!!
 	return c.ModbusClient().ReadDiscreteInputs(address, quantity)
 }
 
 func (c *Connection) WriteMultipleCoils(address, quantity uint16, value []byte) (results []byte, err error) {
 	c.prepare()
+	defer c.Connection.Close() //!!!!
 	return c.ModbusClient().WriteMultipleCoils(address, quantity, value)
 }
 
 func (c *Connection) ReadWriteMultipleRegisters(readAddress, readQuantity, writeAddress, writeQuantity uint16, value []byte) (results []byte, err error) {
 	c.prepare()
+	defer c.Connection.Close() //!!!!
 	return c.ModbusClient().ReadWriteMultipleRegisters(readAddress, readQuantity, writeAddress, writeQuantity, value)
 }
 
 func (c *Connection) MaskWriteRegister(address, andMask, orMask uint16) (results []byte, err error) {
 	c.prepare()
+	defer c.Connection.Close() //!!!!
 	return c.ModbusClient().MaskWriteRegister(address, andMask, orMask)
 }
 
 func (c *Connection) ReadFIFOQueue(address uint16) (results []byte, err error) {
 	c.prepare()
+	defer c.Connection.Close() //!!!!
 	return c.ModbusClient().ReadFIFOQueue(address)
 }
